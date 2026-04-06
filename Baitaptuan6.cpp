2ffbc1bb3e213db9d275e03646baa5ca02a3694d
ĐẶNG VĂN DUY <Duydang792006@gmail.com>
2026-04-06 10:31:20 +0700

    Thay doi cach xoa file

--- Baitaptuan6.cpp
+++ Baitaptuan6.cpp
@@ -33,34 +33,6 @@ void Insertfile(Node*& head, File newFileData) {
 }
 
 
-//Chèn node mới vào danh sách theo thứ tự kích thước(nhỏ đến lớn)
-void InsertSize(Node*& head, File newFileData) {
-    Node* Newnode = new Node;      // Tạo Node mới và gán dữ liệu
-    Newnode->data = newFileData;
-    Newnode->link = nullptr;
-
-    
-    if (head == nullptr || newFileData.size < head->data.size) { // check xem ds có rỗng hoặc file mới nhỏ hơn file đứng đầu
-        Newnode->link = head;
-        head = Newnode;           // Thay thế file mới(newnode) vào đầu nếu đúng
-        return;    }
-    // Tìm vị trí để chèn vào
-    Node* p = head;
-    while (p->link != nullptr && p->link->data.size < newFileData.size) p = p->link;
-  
-    Newnode->link = p->link;     // Chèn node mới vào sau node 'p' đang duyệt
-    p->link = Newnode;
-}
-
-// Hàm sao chép dữ liệu từ List 1 sang List 2 và sắp xếp theo Size
-void CopyListandSort(Node* headSource, Node*& headDest) {
-    Node* q = headSource; 
-    while (q != nullptr) {
-        InsertSize(headDest, q->data); 
-        q = q->link;
-    }
-}
-
 //Hàm tính tổng dung lương(kích thước) các file
 double Sumsize(Node* head) {
     double sum = 0;
@@ -81,11 +53,43 @@ void printList(Node* head) {
 }
 
 
+// Xóa file nhỏ nhất trong list
+double Removesmallest(Node*& head) {
+    if (head == nullptr) return 0; 
+
+    double Minsize = head->data.size;
+    Node* temp = head;
+    while (temp != nullptr) {     //Tìm dung luong nhỏ nhất trong list
+        if (temp->data.size < Minsize) {
+            Minsize = temp->data.size;
+        }
+        temp = temp->link; }
+
+    Node* curr = head;  // su dung 2 con tro liền nhau để xóa file
+    Node* prev = nullptr; 
+
+    while (curr != nullptr) {
+        if (curr->data.size == Minsize) {     // Tìm vị trí file nhỏ nhất 
+            
+            cout << "  Da xoa: " << curr->data.name << " (" << curr->data.size << "GB)" << endl;
+            if (prev == nullptr)  head = curr->link; 
+            else   prev->link = curr->link; 
+            double Size = curr->data.size;
+            delete curr; 
+            return Size;
+        }
+        
+        prev = curr;       
+        curr = curr->link; 
+    }
+    return 0;
+}
+
 
 
 int main() {
     Node* Document = nullptr;
-    Node* Document2 = nullptr; 
+   
     // Thêm file vào danh sách
     Insertfile(Document, {"File so 1", 8.5, 1001});
     Insertfile(Document, {"File so 7", 7.0, 1005});
@@ -104,22 +108,19 @@ int main() {
     cout << "Tong dung luong ban dau: " << Sumsize(Document) << "GB" ;
     if(Sumsize(Document)>32.0) cout << "( > 32Gb) \n" << endl;
     if(Sumsize(Document)<32.0) cout << "( < 32Gb) \n" << endl;
-    CopyListandSort(Document, Document2);
+   // CopyListandSort(Document, Document2);
 
     cout << "Xoa cac file nho nhat de dam bao dung luong <32Gb " << endl;
-     double sum = Sumsize(Document2);
 
-    while (Document2 != nullptr && sum > 32.0) {    // Duyệt Document2 để xóa đảm bảo sumsize<32gb
-        Node* temp = Document2;
-        cout << " Xoa file:  " << temp->data.name << " (" << temp->data.size << "GB)" << endl;
-        sum -= temp->data.size;   // xóa các file 
-        Document2 = Document2->link;
-        delete temp;   }
+    double sum = Sumsize(Document);
+    while (Document != nullptr && sum > 32.0) {
+        sum -= Removesmallest(Document);  // 
+    }
 
   // In ra danh sach con lai
     cout << "\n Danh sach cac file con lai sau khi xoa (Dung luong <32Gb): " << endl;
-     printList(Document2);
-    cout << "Tong dung luong con lai: " << Sumsize(Document2) << " GB" << endl;
-    
+     printList(Document);
+    cout << "Tong dung luong con lai: " << Sumsize(Document) << " GB" << endl;
+ 
     return 0;
 }
